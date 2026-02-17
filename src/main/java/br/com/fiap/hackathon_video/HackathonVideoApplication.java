d34d7afb2798ab0c8f8979e57ebcddc781925841
Pedro <pedro.argentatii@gmail.com>
2026-02-17 12:29:22 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/hackathon_video/HackathonVideoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/hackathon_video/HackathonVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
